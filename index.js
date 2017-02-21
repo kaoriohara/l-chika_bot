f701e43e0ec308d62b4813b363be9e70daaf94e5
kaoriohara <kaorin10815@gmail.com>
2017-02-22 00:38:21 +0900

    change setting

---
+++ index.js
@@ -0,0 +1,17 @@
+vvar http = require('http');
+var fs = require('fs');
+ 
+var server = http.createServer();
+server.on('request', doRequest);
+server.listen(1234);
+console.log('Server running!');
+ 
+// リクエストの処理
+function doRequest(req, res) {
+    fs.readFile('./index.html', 'UTF-8', 
+        function(err, data) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            res.end();
+        });
+}
